846abef7b1374197ee9f28df7ee4c5fc9ccaf47e
Yuki Omori <yuki.omori@inte.co.jp>
2017-04-29 21:28:24 +0900

    add and fix specific codes of adder

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
 const add = require('./adder.js');
 
-class ViewManager = {
+class ViewManager  {
   
-  connectEventHandlers() = {
+  connectEventHandlers() {
   	//wire up event handler for form submit
+  	document.getElementById('form-numbers')
+  	  .addEventListener(
+  	  	  'submit',
+  	  	  this.onSubmit);
   }
 
-  onSubmit(event) = {
+  onSubmit(event) {
     //block form from actually submitting
     // (which would refresh the page)
+    event.preventDefault();
 
     // grab the number values as strings
+    let num1 = document.getElementById('input-num1').value;
+    let num2 = document.getElementById('input-num2').value;
 
     // cast the strings to ints
+    num1 = parseInt(num1, 10);
+    num2 = parseInt(num2, 10);
 
     //add the numbers
+    const sum = add(num1, num2);
 
     //output
+    alsert(sum);
   }
 
 }
 
 const viewManager = new ViewManager();
-viwManager.connectEventHandlers();
+viewManager.connectEventHandlers();
